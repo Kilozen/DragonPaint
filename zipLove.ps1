26bc1db9daec211ca60cb37138561c64c8f228d2
Kilozen <Kilozen@users.noreply.github.com>
2022-09-06 13:50:50 -0700

    update to include ColorListSelector library

--- zipLove.ps1
+++ zipLove.ps1
@@ -1,12 +1,16 @@
 # Script (powershell) to package DragonPaint files as a .love file 
 
+# 9/5/22 - update to include ColorListSelector library. 
+
 write-host "`n"
+
 # .zip all necessary project files together: 
 # Compress-Archive -LiteralPath .\main.lua, .\DragonPaint.lua, .\conf.lua, .\strict.lua, .\images -CompressionLevel Optimal -DestinationPath .\DragonPaint.zip 
 
 # multi-line style (more readable)
 $compress = @{
-    LiteralPath      = ".\main.lua", ".\DragonPaint.lua", ".\conf.lua", ".\strict.lua", ".\images"
+    LiteralPath      = ".\main.lua", ".\DragonPaint.lua", ".\conf.lua", ".\images", ".\lib\strict.lua", 
+    ".\lib\ColorListSelector\ColorListSelector.lua", ".\lib\ColorListSelector\ColorListConfig.lua", ".\ColorListConfig.lua"
     CompressionLevel = "Optimal"
     DestinationPath  = ".\DragonPaint.zip"
 }
